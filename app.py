dbb8786353dcb5bbac5d8a12753e791f085801a6
Naman Gupta <namangupta.cs@gmail.com>
2024-04-01 14:28:31 +0530

    All required files are pushed to GIt

---
+++ app.py
@@ -0,0 +1,70 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import base64
+import io
+import streamlit as st
+import os
+from PIL import Image
+import PyPDF2 as pdf2
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
+
+
+def input_pdf_to_text(uploaded_file):
+    if uploaded_file is not None:
+        ## Convert the PDF to image
+        reader=pdf2.PdfReader(uploaded_file)
+        text = ""
+
+        for page in range(len(reader.pages)):
+            page = reader.pages[page]
+            text+=str(page.extract_text())
+        return text 
+
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+
+## Prompt 
+
+input_prompt = """
+Hey Act like a skilled and very experience ATS(Application Tracking System)
+with the deep understanding of tech field, software engineering, data science, data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide best assistance
+for imporving the resume. Assign the percentage Match based on JD  and the missing keywords 
+with high accuracy.
+resume:{text}
+description:{jd}
+
+I want the response in the single string having the structure as 
+JD Match : %
+MissingKeywords: keywords in bullets
+
+Profile Summary : "" 
+"""
+## streamlit app
+st.set_page_config(page_title="ATS Resume Expert")
+st.header("ATS Tracking System")
+jd=st.text_area("Job Description: ")
+uploaded_file=st.file_uploader("Upload Your Resume",type=["pdf"],help="Please upload the PDF")
+
+if uploaded_file is not None:
+    st.write("PDF Uploaded Successfully")
+
+submit = st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text=input_pdf_to_text(uploaded_file)
+        response=get_gemini_response(input_prompt)
+        st.subheader(response)
+    else:
+        st.write("Please uplaod the resume")
